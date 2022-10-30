7392300568cf7536ef54b61a96885c1563871387
Niklas Andersson <niklas.andersson@iths.se>
2022-10-30 10:02:15 +0100

    Add config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { PeopleModule } from './people/people.module';
 
 @Module({
-  imports: [PrismaModule, PeopleModule],
+  imports: [
+    PrismaModule,
+    PeopleModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
   controllers: [],
   providers: [],
 })
