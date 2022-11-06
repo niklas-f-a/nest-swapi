27e8debdf6147e13b76fd3db678e74336c00d6a0
Niklas Andersson <niklas.andersson@iths.se>
2022-11-06 15:24:26 +0100

    Add planet feature

--- src/planets/planets.controller.ts
+++ src/planets/planets.controller.ts
@@ -1,16 +1,46 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { Planet } from '@prisma/client';
+import { FindAllQueryDto } from 'src/dto';
+import { FindOnePlanetDto } from './dto';
 import { PlanetService } from './planets.service';
 
 @Controller('planets')
 export class PlanetsController {
   constructor(private planetService: PlanetService) {}
 
-
-
+  @Get()
+  findAll(@Query() query: FindAllQueryDto) {
+    return this.planetService.findAll(query);
+  }
 
   @Get(':id')
-  findOne(@Param() id: number, ): Promise<Planet> {
-    return this.planetService.findOne(id);
+  findOne(
+    @Param('id') id: string,
+    @Query() query: FindOnePlanetDto,
+  ): Promise<Planet> {
+    return this.planetService.findOne(id, query);
+  }
+
+  @Patch(':id')
+  update() {
+    return this.planetService.update();
+  }
+
+  @Delete(':id')
+  delete() {
+    return this.planetService.delete();
+  }
+
+  @Post()
+  create() {
+    return this.planetService.create();
   }
 }
--- src/planets/planets.service.ts
+++ src/planets/planets.service.ts
@@ -1,10 +1,42 @@
 import { Injectable } from '@nestjs/common';
+import { FindAllQueryDto } from 'src/dto';
+import { HelperService } from 'src/lib/helper.service';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { FindOnePlanetDto } from './dto';
 
 @Injectable()
 export class PlanetService {
-  constructor(private prisma: PrismaService) {}
-  async findOne(id: number) {
-    return this.prisma.planet.findFirst();
+  constructor(private prisma: PrismaService, private helper: HelperService) {}
+  async findAll(query: FindAllQueryDto) {
+    const count = await this.prisma.planet.count();
+    const take = this.helper.checkLimit(query?.limit);
+    const skip = this.helper.checkPage(query?.page);
+
+    return {
+      count,
+      planets: this.prisma.planet.findMany({ skip, take }),
+    };
+  }
+
+  async findOne(id: string, query: FindOnePlanetDto) {
+    const includeQuery = this.helper.includeQuery(query);
+    console.log(includeQuery);
+
+    return this.prisma.planet.findUnique({
+      where: { id },
+      include: this.helper.isObjectEmpty(includeQuery) ? null : includeQuery,
+    });
+  }
+
+  async update() {
+    return 'hell'
+  }
+
+  async delete() {
+    return 'bing'
+  }
+
+  async create() {
+    return 'bdbdbdbdbd'
   }
 }
